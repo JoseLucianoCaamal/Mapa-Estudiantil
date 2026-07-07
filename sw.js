c0acaee63944b2c3e52a247f71325589f43eeac1
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-07-06 23:59:49 -0600

    2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'mapa-estudiantil-v1';
+const CACHE_NAME = 'mapa-estudiantil-v2'; 
+
 const urlsToCache = [
   './',
   './index.html',
@@ -11,13 +12,36 @@ const urlsToCache = [
   './js/transporte.js'
 ];
 
+// 1. INSTALACIÓN
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  // Fuerza al Service Worker a instalarse de inmediato sin esperar
+  self.skipWaiting(); 
 });
 
+// 2. ACTIVACIÓN (NUEVO: El destructor de basura)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // Si el nombre del caché guardado no coincide con la versión actual, lo borra
+          if (cacheName !== CACHE_NAME) {
+            console.log('🧹 Borrando versión antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Toma el control de la aplicación de inmediato
+  self.clients.claim();
+});
+
+// 3. INTERCEPTOR DE RED
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
