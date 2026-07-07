1930c1b6bbce2d31a0aa1200e5ecdff04657b4fa
JOSELCA <82686845+JoseLucianoCaamal@users.noreply.github.com>
2026-07-06 18:17:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ import { inicializarMapa } from './mapa.js';
 import { cargarEscuelas } from './api.js';
 import { inicializarRutas } from './transporte.js';
 import { listaMarcadores } from './datos.js';
+// 1. Agregamos la importación de tu módulo de ubicación
+import { activarGeolocalizacion } from './ubicacion.js'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const mapa = inicializarMapa();
     
     inicializarRutas(mapa); // Prepara la función window.dibujarRuta
     cargarEscuelas(mapa);   // Carga los pines y botones
+    
+    // 2. Activamos la función conectándola a tu mapa
+    activarGeolocalizacion(mapa);
     
     const input = document.getElementById('buscador');
     input.addEventListener('input', (e) => {
