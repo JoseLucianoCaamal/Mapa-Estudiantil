7a8de1ff47c8c7cc2a83a632faaa3520d5911f67
José Luciano Caamal Ayala <caamalayala8@gmail.com>
2026-07-08 19:23:51 -0600

    8.3

--- js/transporte.js
+++ js/transporte.js
@@ -14,7 +14,7 @@ const coloresRutas = {
     '50_Penal': 'hsl(300, 100%, 50%)',
     'Xmatkuil': 'hsl(45, 100%, 50%)',
     '11_Zazil_Ha_San_Jose': 'hsl(335, 100%, 38%)', 
-    '50_Sur_Villa_Magna': 'hsl(210, 100%, 50%)',
+    '50_Sur_Villa_Magna': 'hsl(s210, 100%, 50%)',
     '42_Sur_Leona_Vicario': 'hsl(204, 100%, 50%)',
     '42_Sur_Cielo_Alto': 'hsl(120, 100%, 50%)',
     '42_Cruz_Roja_Salvador_Alvarado': 'hsl(300, 100%, 50%)',
